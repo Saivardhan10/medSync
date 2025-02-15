84d2c1388b8ff1682a5c8d9de5832e28b0946afc
Saivardhan Nuguri <165569806+Saivardhan10@users.noreply.github.com>
2025-02-16 00:40:42 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           <li className='border px-5 text-xs py-1.5 rounded-full border-black'>Admin/Doctor Login</li>
         </NavLink>
         <NavLink target='_blank' to='https://ai-based-doctor-recommendation-system.streamlit.app/' >
-          <li className='bg-[#b163ff] text-white border px-5 text-xs py-1.5 rounded-full border-black'>MedSync.AI</li>
+          <li className='bg-[#87CEEB] text-black border px-5 text-xs py-1.5 rounded-full border-none'>MedSync.AI</li>
         </NavLink>
       </ul>
 
